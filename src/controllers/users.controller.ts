f18e4dc625664aebd6b988ad205e4b1d7d7838fa
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2026-01-21 15:29:57 -0300

    feat: add a patch user feature

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { iUser } from "../interfaces/user.interface";
 import createUserService from "../services/users/createUser.service";
+import { hashSync } from "bcryptjs";
+import { AppError } from "../errors";
+import patchUserService from "../services/users/patchUser.service";
 
 export const createUserController = async (
   req: Request,
@@ -11,4 +14,28 @@ export const createUserController = async (
   const newUser = await createUserService(userData);
 
   return res.status(201).json(newUser);
+};
+
+export const patchUserController = async (
+  req: Request,
+  res: Response,
+): Promise<Response> => {
+  const { id } = req;
+  if (id !== req.params.id) {
+    throw new AppError("tu só pode editar tua própria conta, como tu chegou até aqui?", 401);
+  }
+
+  let newPassword = req.body.password;
+  let updatedData = { ...req.body };
+
+  if (newPassword) {
+    newPassword = hashSync(newPassword, 10);
+    updatedData.password = newPassword;
+  } else {
+    delete updatedData.password;
+  }
+
+  const user = await patchUserService(updatedData, id);
+
+  return res.status(200).json(user);
 };
--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import ensureDataIsValidMiddleware from "../middlewares/ensureDataIsValid.middleware";
 import { userSchema } from "../schemas/users.schema";
-import { createUserController } from "../controllers/users.controller";
-
+import { createUserController, patchUserController } from "../controllers/users.controller";
+import ensureUserIsAuthenticatedMiddleware from "../middlewares/ensureUserIsAuthenticated.middleware";
 
 const userRoutes: Router = Router();
 
@@ -10,6 +10,12 @@ userRoutes.post(
   "",
   ensureDataIsValidMiddleware(userSchema),
   createUserController,
+);
+
+userRoutes.patch(
+  "/:id",
+  ensureUserIsAuthenticatedMiddleware,
+  patchUserController,
 );
 
 export default userRoutes;
