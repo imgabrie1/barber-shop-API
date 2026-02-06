93dc3ff76bb84078b8c0d5331d3e38cd48ab8235
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2026-02-06 19:27:26 -0300

    feat: barber and admin can change appoitments status and user can cancel your own appoitments

---
+++ src/services/appointments/userCancelled.service.ts
@@ -0,0 +1,53 @@
+import { AppDataSource } from "../../data-source";
+import { Appointment } from "../../entities/appointments.entity";
+import appointmentStatusEnum from "../../enum/appointmentStatus.enum";
+import { AppError } from "../../errors";
+import { returnAppointmentSchema } from "../../schemas/appointments.schema";
+
+const userCancelAppointmentService = async (
+  appointmentID: string,
+  userID: string,
+) => {
+  const appointmentRepo = AppDataSource.getRepository(Appointment);
+  const appointment = await appointmentRepo
+    .createQueryBuilder("appointment")
+    .leftJoinAndSelect("appointment.barber", "barber")
+    .leftJoinAndSelect("appointment.client", "client")
+    .leftJoinAndSelect("appointment.appointmentServices", "as")
+    .leftJoinAndSelect("as.service", "service")
+    .where("appointment.id = :appointmentID", { appointmentID })
+    .getOne();
+
+  if (!appointment) {
+    throw new AppError("Agendamento não encontrado", 404);
+  }
+
+  if (appointment.client.id !== userID) {
+    throw new AppError(
+      "Você não tem permissão para cancelar este agendamento",
+      403,
+    );
+  }
+
+  if (
+    appointment.status !== appointmentStatusEnum.PENDING &&
+    appointment.status !== appointmentStatusEnum.CONFIRMED
+  ) {
+    throw new AppError(
+      `Não é possível cancelar um agendamento com status: ${appointment.status}`,
+      400,
+    );
+  }
+
+  appointment.status = appointmentStatusEnum.CANCELLED;
+  await appointmentRepo.save(appointment);
+
+  const formattedAppointment = {
+    ...appointment,
+    services: appointment.appointmentServices.map((as) => as.service),
+  };
+
+  return returnAppointmentSchema.parse(formattedAppointment);
+};
+
+export default userCancelAppointmentService;
