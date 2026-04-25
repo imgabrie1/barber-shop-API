4c7933c4fc8d366dd5444ccb607388da34c326b3
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2026-04-25 15:07:50 -0300

    fix: add id in params and move to admin routers archive

--- src/routers/admin.routes.ts
+++ src/routers/admin.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { getRevenueController } from "../controllers/admin.controller";
 import ensureIsAdminMiddleware from "../middlewares/ensureIsAdmin.middleware";
 import ensureUserIsAuthenticatedMiddleware from "../middlewares/ensureUserIsAuthenticated.middleware";
+import { adminDeleteUserController } from "../controllers/users.controller";
 
 const adminRoutes = Router();
 
@@ -10,6 +11,13 @@ adminRoutes.get(
   ensureUserIsAuthenticatedMiddleware,
   ensureIsAdminMiddleware,
   getRevenueController,
+);
+
+adminRoutes.delete(
+  "/:id",
+  ensureUserIsAuthenticatedMiddleware,
+  ensureIsAdminMiddleware,
+  adminDeleteUserController,
 );
 
 export default adminRoutes;
