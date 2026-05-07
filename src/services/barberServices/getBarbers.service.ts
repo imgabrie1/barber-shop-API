9c2b0cd1ab456566675065a4d3ce96b4420ea74a
Gabriel Andrade <gabrielcoloradogvda@gmail.com>
2026-05-07 15:43:42 -0300

    feat: add get barber only with shopId

--- src/services/barberServices/getBarbers.service.ts
+++ src/services/barberServices/getBarbers.service.ts
@@ -4,11 +4,14 @@ import roleEnum from "../../enum/role.enum";
 import { iUserReturn } from "../../interfaces/user.interface";
 import { returnMultipleUserSchema } from "../../schemas/users.schema";
 
-const getBarbersService = async (): Promise<iUserReturn[]> => {
+const getBarbersService = async (shopId: string): Promise<iUserReturn[]> => {
   const userRepo = AppDataSource.getRepository(User);
   const barbers = await userRepo.find({
     where: {
       role: roleEnum.BARBER,
+      shop: {
+        id: shopId
+      }
     },
     relations: {
       shop: true,
